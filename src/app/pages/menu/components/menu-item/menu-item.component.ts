17e6082227677821f08edf7f24f910efec615ba8
FranciscoGTIM <francisco.perez@grupotimexico.com>
2024-09-12 18:22:53 -0600

    SweetAlert implementacion

--- src/app/pages/menu/components/menu-item/menu-item.component.ts
+++ src/app/pages/menu/components/menu-item/menu-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-menu-item',
@@ -10,10 +11,19 @@ export class MenuItemComponent implements OnInit {
   @Input() menuTitle: string = '';
   @Input() menuPrice: string = '';
   @Input() hasGuarniciones: boolean = false;
+  @Input() imgLink: string = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openImage(img: string) {
+    Swal.fire({
+      imageUrl: img,
+      imageAlt: "Imagen dañada, disculpe los inconvenientes",
+      showConfirmButton: false,
+    });
   }
 
 }
